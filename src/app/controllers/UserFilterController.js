147cbec9c14be344cacdee91cf6fdca84d109468
YMG <y.munizgonzalez@gmail.com>
2020-04-27 00:59:44 -0300

    chore: ajuste em UserRolerController

--- src/app/controllers/UserFilterController.js
+++ src/app/controllers/UserFilterController.js
@@ -31,34 +31,38 @@ class UserFilterController {
 
         const values = [];
         const idRoles = [];
+        const idRolesNot = [];
 
         for (let x = 0; x < user.roles.length; x++) {
             values.push(user.roles[x].name);
         }
 
         if (values.includes('STUDENT')) {
-            idRoles.push(0);
+            idRoles.push(-1);
         }
         if (values.includes('TEACHER')) {
             idRoles.length = 0;
-            idRoles.push(0);
+            idRoles.push(-1);
         }
 
         if (values.includes('ADMIN')) {
             idRoles.length = 0;
             idRoles.push(2, 3);
+            idRolesNot.push('0', '1', '6');
         }
         if (values.includes('ADMIN+')) {
             idRoles.length = 0;
             idRoles.push(1, 2, 3);
+            idRolesNot.push('0', '6');
         }
 
         if (values.includes('SUPER')) {
             idRoles.length = 0;
             idRoles.push(1, 2, 3, 6);
+            idRolesNot.push('');
         }
 
-        if (idRoles.includes(0)) {
+        if (idRoles.includes(-1)) {
             return res.status(403).json({
                 error: 'Student or teacher user does not have permission.',
             });
@@ -69,13 +73,12 @@ class UserFilterController {
                 message: 'User does not have any compatible roles.',
             });
         }
-
         const users = await ApplicationUsers.findAll({
             where: {
                 email: {
                     [Sequelize.Op.like]: `%` + email + `%`,
                 },
-                '$roles.id$': idRoles,
+                // '$roles.id$': idRoles,
             },
             attributes: ['id', 'userName'],
             include: [
@@ -95,7 +98,19 @@ class UserFilterController {
             ],
         });
 
-        return res.json(users);
+        const usersFilter = [];
+        const r = [];
+
+        users.forEach(function (u) {
+            for (let x = 0; x < u.roles.length; x++) {
+                r.push(u.roles[x].id);
+            }
+            if (!u.roles.some((e) => idRolesNot.includes(e.id))) {
+                usersFilter.push(u);
+            }
+        });
+
+        return res.json(usersFilter);
     }
 }
 
--- src/app/controllers/UserRoleController.js
+++ src/app/controllers/UserRoleController.js
@@ -7,7 +7,7 @@ import IdentityUserRoles from '../models/IdentityUserRoles';
 
 class UserRoleController {
     async index(req, res) {
-        console.log(req);
+        console.log(req.body);
         const { email } = req.body;
 
         const user = await ApplicationUsers.findOne({
@@ -34,34 +34,38 @@ class UserRoleController {
 
         const values = [];
         const idRoles = [];
+        const idRolesNot = [];
 
         for (let x = 0; x < user.roles.length; x++) {
             values.push(user.roles[x].name);
         }
 
         if (values.includes('STUDENT')) {
-            idRoles.push(0);
+            idRoles.push(-1);
         }
         if (values.includes('TEACHER')) {
             idRoles.length = 0;
-            idRoles.push(0);
+            idRoles.push(-1);
         }
 
         if (values.includes('ADMIN')) {
             idRoles.length = 0;
             idRoles.push(2, 3);
+            idRolesNot.push('0', '1', '6');
         }
         if (values.includes('ADMIN+')) {
             idRoles.length = 0;
             idRoles.push(1, 2, 3);
+            idRolesNot.push('0', '6');
         }
 
         if (values.includes('SUPER')) {
             idRoles.length = 0;
             idRoles.push(1, 2, 3, 6);
+            idRolesNot.push('');
         }
 
-        if (idRoles.includes(0)) {
+        if (idRoles.includes(-1)) {
             return res.status(403).json({
                 error: 'Student or teacher user does not have permission.',
             });
@@ -78,7 +82,48 @@ class UserRoleController {
                 email: {
                     [Sequelize.Op.like]: `%` + email + `%`,
                 },
-                '$roles.id$': idRoles,
+                // '$roles.id$': {
+                //     [Sequelize.Op.in]: idRoles,
+                // },
+            },
+            attributes: ['id', 'userName'],
+            include: [
+                {
+                    model: UserProfiles,
+                    as: 'userProfile',
+                    attributes: ['id', 'userFirstName'],
+                },
+                {
+                    model: IdentityRoles,
+                    as: 'roles',
+                    attributes: ['id', 'name'],
+                    through: {
+                        attributes: [],
+                    },
+                },
+            ],
+        });
+        const usersFilter = [];
+        const r = [];
+
+        users.forEach(function (u) {
+            for (let x = 0; x < u.roles.length; x++) {
+                r.push(u.roles[x].id);
+            }
+            if (!u.roles.some((e) => idRolesNot.includes(e.id))) {
+                usersFilter.push(u);
+            }
+        });
+
+        return res.json(usersFilter);
+    }
+
+    async store(req, res) {
+        const { userId, roles } = req.body;
+
+        const user = await ApplicationUsers.findOne({
+            where: {
+                userProfileId: req.userId,
             },
             attributes: ['id', 'userName'],
             include: [
@@ -98,11 +143,146 @@ class UserRoleController {
             ],
         });
 
-        return res.json(users);
-    }
+        const userRequest = await ApplicationUsers.findOne({
+            where: {
+                id: userId,
+            },
+            attributes: ['id', 'userName'],
+            include: [
+                {
+                    model: UserProfiles,
+                    as: 'userProfile',
+                    attributes: ['id', 'userFirstName'],
+                },
+                {
+                    model: IdentityRoles,
+                    as: 'roles',
+                    attributes: ['id', 'name'],
+                    through: {
+                        attributes: [],
+                    },
+                },
+            ],
+        });
 
-    async store(req, res) {
-        const { userId, roles } = req.body;
+        if (
+            user.roles.some((e) => ['ADMIN'].includes(e.name)) &&
+            userRequest.roles.some((e) =>
+                ['ADMIN', 'ADMIN+', 'SUPER'].includes(e.name)
+            )
+        ) {
+            return res.status(403).json({ error: 'You don´t have permission' });
+        }
+        if (
+            user.roles.some((e) => ['ADMIN+'].includes(e.name)) &&
+            userRequest.roles.some((e) => ['ADMIN+', 'SUPER'].includes(e.name))
+        ) {
+            return res.status(403).json({ error: 'You don´t have permission' });
+        }
+
+        if (
+            user.roles.some((e) => ['STUDENT'].includes(e.name)) &&
+            user.roles.length === 1
+        ) {
+            return res.status(403).json({ error: 'You don´t have permission' });
+        }
+
+        if (
+            user.roles.every((e) => ['STUDENT', 'TEACHER'].includes(e.name)) &&
+            user.roles.length === 2
+        ) {
+            return res.status(403).json({ error: 'You don´t have permission' });
+        }
+
+        //---------------------------------------------------------------------------
+
+        if (
+            user.roles.some((e) => ['ADMIN'].includes(e.name)) &&
+            roles.some((e) => [1, 6, 0].includes(e.id))
+        ) {
+            return res.status(403).json({ error: 'You don´t have permission' });
+        }
+
+        if (
+            user.roles.some((e) => ['ADMIN+'].includes(e.name)) &&
+            roles.some((e) => [6, 0].includes(e.id))
+        ) {
+            return res.status(403).json({ error: 'You don´t have permission' });
+        }
+
+        const newRoles = roles.filter(function (e, i) {
+            return roles.indexOf(e) === i;
+        });
+
+        // const roleUserRequestAdminMore = newRoles.some((e) =>
+        //     [6].includes(e.id)
+        // );
+        // const roleUserRequestSuper = newRoles.some((e) => [0].includes(e.id));
+        // const roleUserRequestAdmin = newRoles.some((e) => [1].includes(e.id));
+        // const roleUserRequestTeacher = newRoles.some((e) => [2].includes(e.id));
+        // const roleUserRequestStudent = newRoles.some((e) => [3].includes(e.id));
+
+        // const authorizateLoggedSuper = user.roles.some((e) =>
+        //     ['SUPER'].includes(e.name)
+        // );
+
+        // const authorizateLoggedAdminMore = user.roles.some((e) =>
+        //     ['SUPER', 'ADMIN+'].includes(e.name)
+        // );
+        // console.log(authorizateLoggedAdminMore);
+
+        // const authorizateLoggedAdmin = user.roles.some((e) =>
+        //     ['SUPER', 'ADMIN+', 'ADMIN'].includes(e.name)
+        // );
+
+        // const authorizateLoggedStudent =
+        //     user.roles.some((e) => ['STUDENT'].includes(e.name)) &&
+        //     user.roles.length === 1;
+
+        // const authorizateLoggedTeacher =
+        //     user.roles.every((e) => ['STUDENT', 'TEACHER'].includes(e.name)) &&
+        //     user.roles.length === 2;
+
+        // if (authorizateLoggedStudent) {
+        //     return res
+        //         .status(403)
+        //         .json({ error: 'You don´t have permission.' });
+        // }
+        // if (authorizateLoggedTeacher) {
+        //     return res.status(403).json({ error: 'You don´t have permission' });
+        // }
+
+        // if (roleUserRequestStudent) {
+        //     return res
+        //         .status(403)
+        //         .json({ error: 'Student role cannot be changed' });
+        // }
+
+        // if (roleUserRequestSuper) {
+        //     return res.status(403).json({ error: 'You don´t have permission' });
+        // }
+
+        // if (roleUserRequestAdminMore) {
+        //     if (!authorizateLoggedSuper) {
+        //         return res
+        //             .status(403)
+        //             .json({ error: 'You don´t have permission' });
+        //     }
+        // }
+        // if (roleUserRequestAdmin) {
+        //     if (!authorizateLoggedAdminMore) {
+        //         return res
+        //             .status(403)
+        //             .json({ error: 'You don´t have permission' });
+        //     }
+        // }
+        // if (roleUserRequestTeacher) {
+        //     if (!authorizateLoggedAdmin) {
+        //         return res
+        //             .status(403)
+        //             .json({ error: 'You don´t have permission' });
+        //     }
+        // }
 
         await IdentityUserRoles.destroy({
             where: {
@@ -110,17 +290,19 @@ class UserRoleController {
             },
         });
 
+        newRoles.push({ id: 3 });
+
         const userRole = {
             userId,
             roleId: 0,
         };
 
-        await roles.forEach(async (role) => {
+        await newRoles.forEach(async (role) => {
             userRole.roleId = role.id;
             await IdentityUserRoles.create(userRole);
         });
 
-        return res.json({ roles });
+        return res.json({ newRoles });
     }
 }
 
