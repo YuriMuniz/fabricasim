424c3dad0106651296d6b582930327263cce8a08
YMG <y.munizgonzalez@gmail.com>
2020-05-05 12:15:22 -0300

    chore: config db

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,11 @@ module.exports = {
     password: process.env.DB_PASS,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
+    dialectOptions: {
+        options: {
+            encrypt: true,
+        },
+    },
     define: {
         timestamps: false,
     },
