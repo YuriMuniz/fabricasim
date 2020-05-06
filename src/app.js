3eaac05235db7a9b87ead080780235343259e96e
YMG <y.munizgonzalez@gmail.com>
2020-05-06 20:02:42 -0300

    chore: att

--- src/app.js
+++ src/app.js
@@ -12,12 +12,12 @@ class App {
     }
 
     middlewares() {
-        this.server.use(cors());
-        // this.server.use(
-        //     cors({
-        //         origin: 'https://romantic-swanson-d346aa.netlify.app',
-        //     })
-        // );
+        // this.server.use(cors());
+        this.server.use(
+            cors({
+                origin: 'https://portal.fabricasim.com/',
+            })
+        );
         this.server.use(express.json());
     }
 
